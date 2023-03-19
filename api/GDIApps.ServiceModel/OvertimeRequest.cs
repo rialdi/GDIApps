9b3352d1fe99719d4af2f528ceb129ef0542378c
adam <adam.ramdhani@ptgdi.com>
2023-03-19 13:38:09 +0700

    prep backend

---
+++ api/GDIApps.ServiceModel/OvertimeRequest.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GDIApps.ServiceModel.Types.Tables;
+using ServiceStack;
+
+namespace GDIApps.ServiceModel
+{
+      [Route("/createclaim", "POST")]
+    public class CreateOvertimeDraft: IPost, IReturn<CRUDClaimItemResponse>
+    {
+        public string OtDate { get; set; }
+        public List<string> EmployeeIds { get; set; }
+    }
+    public class CRUDClaimItemResponse
+    {
+        public bool Success { get; set; }
+        public string OtNumber { get; set; }
+        public string ErrorMessage { get; set;}
+    }
+    public class CreateOvertimeResponse
+    {
+        public List<CRUDClaimItemResponse> Items { get; set; }=new List<CRUDClaimItemResponse>();
+    }
+
+    [Route("/submitclaim", "POST")]
+    public class SubmitClaimRequest:IReturn<CRUDClaimItemResponse>
+    {
+        public string OtNumber { get; set; }
+        public string ReasonCode { get; set; }
+        public decimal OtHour { get; set; }
+    }
+
+
+    
+ public class UpdateClaimRequest : IReturn<CRUDClaimItemResponse>
+    {
+        public string OTNumber { get; set; }
+        public decimal? OTHour { get; set; }
+        public string ReasonCode { get; set; }
+    }
+
+
+    public class DeleteClaimRequest:IReturn<CRUDClaimItemResponse>
+    {
+        public string OTNumber { get; set; }
+    }
+ 
+    [Route("/OvertimeDraft", "GET")]
+    public class QueryOvertimeDraft : QueryDb<Overtime>
+    {
+        public string Id { get; set; }
+    }
+}
---
+++ api/GDIApps.ServiceModel/Types/Tables/Overtime.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+using ServiceStack;
+using ServiceStack.DataAnnotations;
+
+namespace GDIApps.ServiceModel.Types.Tables
+{
+     [DataContract]
+    [CompositeIndex(true, nameof(OT_NUMBER))]
+    public class Overtime: AuditBase
+    {
+        [AutoIncrement]
+        public int Id { get; set; }
+        [DataMember(Name ="OT_NUMBER")]
+        public string OT_NUMBER { get; set; }
+        [DataMember(Name = "EMPLOYEE_ID")]
+        public string EMPLOYEE_ID { get; set; }
+
+        [DataMember(Name  = "NAME")]
+        public string NAME { get; set; }
+
+        [DataMember(Name ="OT_DATE")]
+        public DateTime OT_DATE { get; set; }
+
+        [DataMember(Name = "OT_HOUR")]
+        public decimal OT_HOUR { get; set; }
+        [DataMember(Name = "OT_REASON")]
+        public string OT_REASON { get; set; }
+        [DataMember(Name = "OT_REASON_CODE")]
+        public string OT_REASON_CODE { get; set; }
+
+        [DataMember(Name = "POS_DEPT")] 
+        public string POS_DEPT { get; set; }
+
+        [DataMember(Name = "POSITION_ID")]
+        public string POSITION_ID { get; set; }
+
+        [DataMember(Name = "FIRST_APPROVER_ID")]
+        public string FIRST_APPROVER_ID { get; set; }
+
+        [DataMember(Name = "FIRST_APPROVER_NAME")]
+        public string FIRST_APPROVER_NAME { get; set; }
+
+        [DataMember(Name = "SECONDARY_APPROVER_ID")]
+        public string SECONDARY_APPROVER_ID { get; set; }
+
+        [DataMember(Name = "SECONDARY_APPROVER_NAME")]
+        public string SECONDARY_APPROVER_NAME { get; set; }
+
+        [DataMember(Name = "STATUS")]
+        public string STATUS { get; set; }
+    }
+}
