03ce7c2c4e8792df05c03ffe2f8582f3578733cf
rialdi <rialdi@ptgdi.com>
2023-12-11 00:38:26 +0700

    Add ProjectPlan

--- api/GDIApps.ServiceModel/Types/Tables/ProjectPlan.cs
+++ api/GDIApps.ServiceModel/Types/Tables/ProjectPlan.cs
@@ -1,79 +1,99 @@
-// using System;
-// using System.Collections.Generic;
-// using ServiceStack;
-// using ServiceStack.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using ServiceStack;
+using ServiceStack.DataAnnotations;
 
-// namespace GDIApps.ServiceModel.Types;
+namespace GDIApps.ServiceModel.Types;
 
-// [CompositeIndex(true, nameof(ProjectId), nameof(AppUserId))]
-// public class ProjectPlan
-// {
-//     [AutoIncrement]
-//     public int Id { get; set; }  
+[CompositeIndex(true, nameof(ProjectId), nameof(VersionNo), nameof(TaskCode))]
+public class ProjectPlan
+{
+    [AutoIncrement]
+    public int Id { get; set; }  
 
-//     [Required]
-//     [References(typeof(ProjectPlanVersion))]
-//     public int ProjectPlanVersionId { get; set;}
+    [Required]
+    [References(typeof(Project))]
+    public int ProjectId { get; set; }
 
-//     [Required]
-//     public string TaskCode { get; set;}
+    [Required]
+    public int VersionNo { get; set; }
 
-//     [Required]
-//     public string TaskTitle { get; set;}
+    [Required]
+    public string TaskCode { get; set; } = string.Empty;
+
+    public string ParentCode { get; set; } = string.Empty;
+
+    [Required]
+    public int TaskLevel { get; set; }
+
+    [Required]
+    public int TaskNo { get; set; }
+
+    [Required]
+    public string TaskTitle { get; set; } = string.Empty;
+    public decimal DurationDays { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+
+    public decimal Percemtage { get; set; }
+
+    public string Assigned { get; set; } = string.Empty;
+    public decimal ResourceCost { get; set; }
+}
 
 
-//     public DateTime StartDate { get; set; }
-//     public DateTime EndDate { get; set; }
-// }
-
-// public class ProjectPlanView : ProjectPlan
-// {
-//     public string? UserName {get; set;}
-//     public string? FirstName {get; set;}
-//     public string? LastName {get; set;}
-//     public string? Email {get; set;}
-// }
-
-// [ValidateIsAuthenticated]
-// [Tag("Projects")]
-// public class QueryProjectPlans : QueryDb<ProjectPlan, ProjectPlanView>, IJoin<ProjectPlan, AppUser>{
-//     public int? ProjectId { get; set;}
-// }
+[ValidateIsAuthenticated]
+[Tag("Projects")]
+public class QueryProjectPlans : QueryDb<ProjectPlan>{
+    public int? ProjectId { get; set;}
+}
 
 
-// [Tag("Projects")]
-// [ValidateIsAuthenticated]
-// public class CreateProjectPlan : ICreateDb<ProjectPlan>, IReturn<CRUDResponse>
-// {
-//     public int ProjectId { get; set; }
-//     public int AppUserId { get; set; }
+[Tag("Projects")]
+[ValidateIsAuthenticated]
+public class CreateProjectPlan : ICreateDb<ProjectPlan>, IReturn<CRUDResponse>
+{
+    public int ProjectId { get; set; }
+    public int VersionNo { get; set; }
+    public string TaskCode { get; set; } = string.Empty;
+    public string ParentCode { get; set; } = string.Empty;
+    public string DependecyTaskCode { get; set; } = string.Empty;
+    public int TaskLevel { get; set; }
+    public int TaskNo { get; set; }
+    public string TaskTitle { get; set; } = string.Empty;
+    public decimal DurationDays { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public decimal Percemtage { get; set; }
+    public string Assigned { get; set; } = string.Empty;
+    public decimal ResourceCost { get; set; }
+}
 
-//     [ApiAllowableValues(typeof(PROJECT_TEAM_ROLE))]
-//     public PROJECT_TEAM_ROLE ProjectPlanRole { get; set; } 
-
-//     [ApiAllowableValues(typeof(CURRENCY_RATE))]
-//     public CURRENCY_RATE ProjectPlanRoleCurrRate { get; set; } 
-//     public decimal ProjectPlanRoleRatePerDay { get; set; }
-// }
-
-// [ValidateIsAuthenticated]
-// [Tag("Projects")]
-// public class UpdateProjectPlan : IPatchDb<ProjectPlan>, IReturn<CRUDResponse>
-// {
-//     public int Id { get; set; } 
-//     public int ProjectId { get; set; }
-//     public int AppUserId { get; set; }
+[ValidateIsAuthenticated]
+[Tag("Projects")]
+public class UpdateProjectPlan : IPatchDb<ProjectPlan>, IReturn<CRUDResponse>
+{
+    public int Id { get; set; } 
+    public int ProjectId { get; set; }
+    public int VersionNo { get; set; }
+    public string TaskCode { get; set; } = string.Empty;
+    public string ParentCode { get; set; } = string.Empty;
+    public string DependecyTaskCode { get; set; } = string.Empty;
+    public int TaskLevel { get; set; }
+    public int TaskNo { get; set; }
+    public string TaskTitle { get; set; } = string.Empty;
+    public decimal DurationDays { get; set; }
     
-//     [ApiAllowableValues(typeof(PROJECT_TEAM_ROLE))]
-//     public PROJECT_TEAM_ROLE ProjectPlanRole { get; set; } 
-//     [ApiAllowableValues(typeof(CURRENCY_RATE))]
-//     public CURRENCY_RATE ProjectPlanRoleCurrRate { get; set; } 
-//     public decimal ProjectPlanRoleRatePerDay { get; set; }
-// }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public decimal Percemtage { get; set; }
+    public string Assigned { get; set; } = string.Empty;
+    public decimal ResourceCost { get; set; }
+}
 
-// [ValidateIsAuthenticated]
-// [Tag("Projects")]
-// public class DeleteProjectPlan : IDeleteDb<Project>, IReturnVoid
-// {
-//     public int Id { get; set; }        
-// }
+[ValidateIsAuthenticated]
+[Tag("Projects")]
+public class DeleteProjectPlan : IDeleteDb<Project>, IReturnVoid
+{
+    public int Id { get; set; }        
+}
